75a5b7d8ac5ef7b5d16711f315bd84328c346703
Foster Brereton <fbrereto@adobe.com>
2016-04-20 10:16:58 -0700

    adding large grass surface for floor. take one.

--- glfw_shell/main.cpp
+++ glfw_shell/main.cpp
@@ -1477,7 +1477,14 @@ cubeD_D myCube{0,0,0,1,1,1,gTextureBall,gTextureBall,gTextureBall,gTextureBall,g
     
         glTranslatef(camX+2,camY-2.5,camZ);
 
-        
+        gTexture.activate();
+        glBegin(GL_QUADS); // All OpenGL drawing begins with a glBegin.
+        glTexCoord2f(0, 1); glVertex3f(-10, 10, 0.5);
+        glTexCoord2f(1, 1); glVertex3f(10, 10, 0.5);
+        glTexCoord2f(1, 0); glVertex3f(10, -10, 0.5);
+        glTexCoord2f(0, 0); glVertex3f(-10, -10, 0.5);
+        glEnd(); // All OpenGL drawing ends with a glEnd.
+
         //If you would like to make a custom make change this to true v
         
         bool customMap{true};
